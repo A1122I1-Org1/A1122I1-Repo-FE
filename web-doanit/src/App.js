6829f539ef357f9ce60b38041768841830597ffb
khoahuyn <khoahuynh2k3hi@gmail.com>
2023-12-10 11:14:15 +0700

    up code by khoa

--- web-doanit/src/App.js
+++ web-doanit/src/App.js
@@ -6,6 +6,7 @@ import {ListStudentAd} from "./component/student/ListStudentAd";
 import {ListStudentTeacher} from "./component/student/ListStudentTeacher";
 import HomePage from "./pages/HomePage";
 import TopicTable from "./component/TopicTable";
+import {Create} from "./component/student/Create-student";
 
 function App() {
     return (
@@ -16,7 +17,7 @@ function App() {
                 <Route path="/student-list" element={<ListStudentAd/>}></Route>
                 <Route path="/student-list-teacher" element={<ListStudentTeacher/>}></Route>
                 <Route path="/topic-table" element={<TopicTable/>}></Route>
-
+                <Route path="/create-student" element={<Create/>}/>
             </Routes>
         </>
     );
